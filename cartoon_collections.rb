730e52a492f60f6d349e0bd5b21db83ac547dc4d
Jerry Bennett <jerrysbusiness@gmail.com>
2017-10-31 20:24:11 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|name, num|
+    puts "#{num + 1}. #{name}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map { |i|
+    i.capitalize + "!"
+  }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+    arr.any? {|word|
+      word.length > 4
+    }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  match = (arr & cheese_types)
+  if match.length != 0
+    match[0].to_s
+  else
+    return nil
+  end
 end
